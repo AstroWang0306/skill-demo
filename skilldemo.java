69972ae5503c08edec6eea4a85f85a86a2c31ee5
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-05-01 18:11:07 -0700

    add one failed test

--- skilldemo.java
+++ skilldemo.java
@@ -1,3 +1,6 @@
 public class skilldemo{
-  Lets go to github desktop
+  //Lets go to github desktop
+  public static int add(int a, int b){
+    return a+b;
+  }
 }
---
+++ skilldemotester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotester{
+
+    @Test
+    public void addtest(){
+        assertEquals(2, skilldemo.add(2,3));
+    }
+
+}
